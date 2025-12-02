37c7b2e58e15985177c0a4898d423b829b1dd9f5
Ivan_Deyev <deyevivan777@mail.ru>
2025-12-02 15:46:55 +0500

    database naming

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_DB_URL = 'sqlite:///./itproger.db'
+SQL_DB_URL = 'sqlite:///./usersposts.db'
 
 engine = create_engine(SQL_DB_URL, connect_args={"check_same_thread": False})
 
